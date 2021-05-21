c9106c113ce7896becbe8bd4d3d64009a222784d
oof1lab <oof1lab@gmail.com>
2021-05-21 18:33:13 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const {XcApis} = require("xc-instant");
-require('dotenv').config();
-const app = require('express')();
-
 (async () => {
-  app.use(await (new XcApis()).init());
-  app.listen(process.env.PORT || 8080);
-  console.log(`App started successfully.\nVisit -> http://localhost:${process.env.PORT || 8080}/xc`);
-})().catch(e => console.log(e))
-
-module.exports = app;
+    const app = require('express')();
+    const {Noco} = require("nocodb");
+    app.use(await Noco.init({}));
+    app.listen(process.env.PORT);
+})()
