c03316b0833131da4d2f914ffdbdfaffb2a61f4b
npgia <pyudroi@gmail.com>
2021-05-26 10:46:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
     const {Noco} = require("nocodb");
     app.use(await Noco.init({}));
     app.listen(process.env.PORT);
-    console.log(`Successfully started at localhost:${process.env.PORT}`)
+    console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
 })()
