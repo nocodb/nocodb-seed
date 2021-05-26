8b6cda44b48cfe9500030f2d80b4940dd8fa3339
npgia <pyudroi@gmail.com>
2021-05-26 10:46:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
     const {Noco} = require("nocodb");
     app.use(await Noco.init({}));
     app.listen(process.env.PORT);
+    console.log(`Successfully started at localhost:${process.env.PORT}`)
 })()
