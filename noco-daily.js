846d43308c6f4bbe5a58e6cc42e782273f154d5a
navi <oof1lab@gmail.com>
2022-05-11 22:59:46 +0100

    Create noco-daily.js

---
+++ noco-daily.js
@@ -0,0 +1,11 @@
+// npm uninstall --save nocodb
+// npm install --save nocodb-daily@0.90.11-20220509-1727
+// node noco-daily.js
+
+// (async () => {
+//     const app = require('express')();
+//     const {Noco} = require("nocodb-daily");
+//     app.use(await Noco.init({}));
+//     console.log(`Visit : localhost:${process.env.PORT}/dashboard`)    
+//     app.listen(process.env.PORT);
+// })()
