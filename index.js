dc4449ff3d95c7f47284d6030181b61b0212f06c
Pranav C <pranavxc@gmail.com>
2022-05-20 17:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (async () => {
     const app = require('express')();
     const {Noco} = require("nocodb");
-    app.use(await Noco.init({}, httpServer, app));
     const httpServer = app.listen(process.env.PORT || 8080);
+    app.use(await Noco.init({}, httpServer, app));
     console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
 })()
