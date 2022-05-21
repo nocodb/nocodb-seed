014ac7da07a83da34219377081e4c0547f3113d3
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-21 11:56:17 +0800

    fix: add try catch block to display error

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 (async () => {
-    const app = require('express')();
-    const {Noco} = require("nocodb");
-    const httpServer = app.listen(process.env.PORT || 8080);
-    app.use(await Noco.init({}, httpServer, app));
-    console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
+    try {
+        const app = require('express')();
+        const {Noco} = require("nocodb");
+        const httpServer = app.listen(process.env.PORT || 8080);
+        app.use(await Noco.init({}, httpServer, app));
+        console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
+    } catch(e) {
+        console.log(e)
+    }
 })()
