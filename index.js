a7e0b96a51ce0889892bd6e24db779e533afe0fc
Pranav C <pranavxc@gmail.com>
2023-02-17 16:05:30 +0530

    chore: remove extra console log which is no longer required

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
         const {Noco} = require("nocodb");
         const httpServer = app.listen(process.env.PORT || 8080);
         app.use(await Noco.init({}, httpServer, app));
-        console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
     } catch(e) {
         console.log(e)
     }
